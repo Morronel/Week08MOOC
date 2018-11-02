87cbe568d0bd68a4acdaaea953be9ea7add313d2
Morronel <zahar.kogut@gmail.com>
2018-11-02 06:34:15 +0200

    Completed ex 12.2

--- src/Storehouse.java
+++ src/Storehouse.java
@@ -3,13 +3,35 @@ import java.util.Map;
 
 public class Storehouse {
     private Map<String, Integer> map;
+    private Map<String, Integer> stockMap;
 
     public Storehouse(){
         this.map = new HashMap<>();
+        this.stockMap = new HashMap<>();
+    }
+
+    public int stock(String product){
+        if (this.stockMap.get(product) != null){
+            return this.stockMap.get(product);
+        }
+        else{
+            return 0;
+        }
+    }
+
+    public boolean take(String product){
+        if (this.stockMap.get(product) != null && this.stockMap.get(product) > 0){
+            this.stockMap.replace(product, this.stockMap.get(product) - 1);
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     public void addProduct(String product, int price, int stock){
         this.map.put(product, price);
+        this.stockMap.put(product, stock);
     }
 
     public int price(String product){
